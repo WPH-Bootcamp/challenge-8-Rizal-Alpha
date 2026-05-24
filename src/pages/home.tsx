f354ca19c45d16dcdcc852d5d06fc1e7afb55782
Rizal-Alpha <projectfalcon21@gmail.com>
2026-05-24 17:40:13 +0800

    tambahkan file cangkang .ts di folder data

---
+++ src/pages/home.tsx
@@ -0,0 +1,21 @@
+export function Home() {
+  return (
+    <div>
+      {/* <Navbar />
+      <main className='space-y-16 md:space-y-24'>
+        <HeroSection />
+        <CompanyProof />
+        <Achievements />
+        <Process />
+        <Services />
+        <Industries />
+        <Projects />
+        <Testimonials />
+        <Supports />
+        <Contacts />
+      </main>
+      <Footer /> */}
+      OKE BRO GASS!!
+    </div>
+  );
+}
